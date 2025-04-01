4adcb88c23f6f4a8f1d75ec5cb5ce2db60354abf
Beatris <beatrisraj7@gmail.com>
2025-04-02 02:08:45 +0300

    Add background

--- src/main.js
+++ src/main.js
@@ -2,17 +2,39 @@ import './style.css'
 import Phaser from 'phaser'
 
 const sizes = {
-	width:500,
-	heigth:500,
+	width:800,
+	height:350,
 }
 
 const speedDown = 300;
 const speedX = 200;
 
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('scene-game');
+        this.leftPlatform;
+		this.rightPlatform;
+        this.cursor;
+		this.wasd;
+        this.platformSpeed = 300; 
+    }
+
+    preload() {
+        this.load.image('bg', '/BG.jpg');
+    }
+
+    create() {
+        this.add.image(0, 0, 'bg').setOrigin(0, 0);
+    }
+
+    update() {
+    }
+}
+
 const config = {
-	type: Phaser.AUTO,
+	type: Phaser.WEBGL,
 	width: sizes.width,
-	heigth:sizes.heigth,
+	height: sizes.height,
 	canvas:gameCanvas,
 	physics:{
 		default:'arcade',
@@ -21,8 +43,10 @@ const config = {
 				x: speedX,
 				y: speedDown
 			},
+			debug: true
 		}
-	}
+	},
+	scene:[GameScene]
 }
 
 const game = new Phaser.Game(config)
