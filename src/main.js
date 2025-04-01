9a963e0a59b441d70d81a491896307fd18c0b440
Beatris <beatrisraj7@gmail.com>
2025-04-02 01:07:46 +0300

    Game config

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 import './style.css'
+import Phaser from 'phaser'
 
+const sizes = {
+	width:500,
+	heigth:500,
+}
 
+const speedDown = 300;
+const speedX = 200;
+
+const config = {
+	type: Phaser.AUTO,
+	width: sizes.width,
+	heigth:sizes.heigth,
+	canvas:gameCanvas,
+	physics:{
+		default:'arcade',
+		arcade:{
+			gravity: {
+				x: speedX,
+				y: speedDown
+			},
+		}
+	}
+}
+
+const game = new Phaser.Game(config)
